20fc1d78dfa79b33665a8a4cfadc0f1a011663cf
ricky@gonzalezstrategy.com <ricky@gonzalezstrategy.com>
2021-12-27 03:30:54 -0600

    locations

--- 12/12.rb
+++ 12/12.rb
@@ -1,14 +1,16 @@
 require 'pry'
 
-
+locations = Hash.new
+data = Array.new
 def get_data
   $data = File.read("input.txt").split("\n")
   $data.map! { |d| d.split("-")}
+  locations = $data.flatten.uniq.map! {|p| [p, []] }.to_h
+  $data.each do |d|
+      locations[d[0]] << d[1]
+      locations[d[1]] << d[0]
+  end
 end
 
 get_data
-points = $data.flatten.uniq
-points.map! {|p| [p, []] }
-locations=points.to_h
-
 binding.pry
