924e7902afa48fe990c2dd3293d2cb8b7e76a6c3
ricky@gonzalezstrategy.com <ricky@gonzalezstrategy.com>
2021-12-31 01:30:03 -0600

    12.1 partial

--- 12/12.rb
+++ 12/12.rb
@@ -1,16 +1,44 @@
 require 'pry'
 
-locations = Hash.new
-data = Array.new
-def get_data
+$locations = Hash.new
+$data = Array.new
+def get_data 
   $data = File.read("input.txt").split("\n")
   $data.map! { |d| d.split("-")}
-  locations = $data.flatten.uniq.map! {|p| [p, []] }.to_h
+  $locations = $data.flatten.uniq.map! {|p| [p, []] }.to_h
   $data.each do |d|
-      locations[d[0]] << d[1]
-      locations[d[1]] << d[0]
+      $locations[d[0]] << d[1]
+      $locations[d[1]] << d[0]
   end
 end
+#If thre's a path forward, take it
+# if there's no path foward, and not end - error
+# if end, return end + point. 
 
+class String
+  def capitalized?
+    self == self.upcase
+  end
+end
+def map_locations point = "start", explored = []
+  paths = []
+  pp "current location: #{point}"
+  if point == "end"
+    ary = ["end"]
+    return ary
+  else
+    explored << point unless point.capitalized?
+    paths = []
+    ($locations[point]-explored).each do |link|
+      path =  map_locations(link, explored)
+      pp path
+     
+    end
+  end
+  return paths
+end
+
+end
 get_data
+map_locations
 binding.pry
